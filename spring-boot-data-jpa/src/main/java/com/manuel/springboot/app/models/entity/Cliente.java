37cc964432c1ab965d77e3b63722814555c9705a
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel-2.local>
2020-01-23 01:41:22 +0100

    Creando estructuras

--- spring-boot-data-jpa/src/main/java/com/manuel/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/manuel/springboot/app/models/entity/Cliente.java
@@ -1,5 +1,10 @@
 package com.manuel.springboot.app.models.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
 public class Cliente {
 
 }
