51f46e9703fd2b4326b45db1cf06c7f4b1422b57
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel-2.local>
2020-01-24 02:12:31 +0100

    Añadiendo paginador

--- spring-boot-data-jpa/src/main/java/com/manuel/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/manuel/springboot/app/models/dao/IClienteDao.java
@@ -1,11 +1,11 @@
 package com.manuel.springboot.app.models.dao;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.manuel.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long>{
 
 	
 	
